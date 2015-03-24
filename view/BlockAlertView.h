81749558a7adbb89c57f6716ce5aee06540a752e
hwj4477 <hwj4477@gmail.com>
2015-03-24 18:31:49 +0900

    add BlockAlertView

---
+++ view/BlockAlertView.h
@@ -0,0 +1,22 @@
+//
+//  BlockAlertView.h
+//
+//  Created by hwj4477 on 2014. 3. 26..
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+typedef void (^clickBlock_t)(NSInteger buttonIndex);
+
+@interface BlockAlertView : NSObject <UIAlertViewDelegate>{
+    clickBlock_t clickBlock_;
+}
+
++ (void)showAlertTitle:(NSString *)title
+               message:(NSString *)message
+     cancelButtonTitle:(NSString *)cancelButtonTitle
+      otherButtonTitle:(NSString *)otherButtonTitle
+            clickBlock:(clickBlock_t)clickBlock;
+
+@end
